1bb7d27e183a1cecef6aa6e82752642270029ae3
philosopher97 <taeho0274@gmail.com>
2021-05-13 02:21:33 +0900

    20210513

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 // JavaScript Document
 
-var x = document.getElementById("myAudio");
-function playAudio() {
-	x.play();
+function Test() {   // eslint-disable-line no-unused-vars
+	var text_bg = document.getElementById("text_bg");
+	var text = document.getElementById("text");
+	//var obj = document.getElementById("obj");
+	text_bg.style.zIndex=3;
+	text.style.zIndex=4;
+	//obj.style.zIndex=-1;
 }
-function stopAudio() {
-	x.pause()
-}
+
