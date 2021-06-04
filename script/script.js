e7d48e0449f98c01816e031c60cc0c999517813b
philosopher97 <taeho0274@gmail.com>
2021-06-04 18:06:35 +0900

    20210604

--- script/script.js
+++ script/script.js
@@ -26,97 +26,80 @@ function  FadeIn_text() {  // eslint-disable-line no-unused-vars
 	x.style.animationPlayState = "running";
 }
 
-
-
-
-
-
 function ShowImages() {  // eslint-disable-line no-unused-vars
 	setTimeout(function() {var img2 = document.getElementById("img2");
-	img2.style.zIndex=3; img2.style.animationPlayState="running"}, 3000);
+	img2.style.zIndex=3; img2.style.animationPlayState="running"}, 1500);
 	
 	setTimeout(function() {var img3 = document.getElementById("img3");
-	img3.style.zIndex=4; img3.style.animationPlayState="running"}, 6000);
+	img3.style.zIndex=4; img3.style.animationPlayState="running"}, 4000);
 	
 	
 	setTimeout(function() {var x = document.getElementById("text"); x.style.zIndex=6;
-	x.style.animationPlayState = "running";}, 9000);
+	x.style.animationPlayState = "running";}, 6500);
 	
 	setTimeout(function() {var x = document.getElementById("text_bg"); x.style.zIndex=5;
-	x.style.animationPlayState = "running";}, 9000);
+	x.style.animationPlayState = "running";}, 6500);
 	
 	setTimeout(function() {var next_btn = document.getElementById("next_btn");
 	next_btn.style.position="relative";
 	next_btn.style.zIndex=4;
 	next_btn.style.top="auto";
-	next_btn.style.left="auto";}, 10000);
+	next_btn.style.left="auto";}, 7500);
 }
-
-
-
-
-
-
 
 function ShowImages2() {  // eslint-disable-line no-unused-vars
 	setTimeout(function() {var img2 = document.getElementById("img2");
-	img2.style.zIndex=3; img2.style.animationPlayState="running"}, 3000);
+	img2.style.zIndex=3; img2.style.animationPlayState="running"}, 1500);
 	
 	setTimeout(function() {var img3 = document.getElementById("img3");
-	img3.style.zIndex=4; img3.style.animationPlayState="running"}, 6000);
+	img3.style.zIndex=4; img3.style.animationPlayState="running"}, 4000);
 	
 	
 	setTimeout(function() {var x = document.getElementById("text"); x.style.zIndex=6;
-	x.style.animationPlayState = "running";}, 9000);
+	x.style.animationPlayState = "running";}, 6500);
 	
 	setTimeout(function() {var x = document.getElementById("text_bg"); x.style.zIndex=5;
-	x.style.animationPlayState = "running";}, 9000);
+	x.style.animationPlayState = "running";}, 6500);
 	
 	setTimeout(function() {var btn = document.getElementById("back_btn");
 	btn.style.position="relative";
 	btn.style.zIndex=4;
 	btn.style.top="auto";
-	btn.style.left="auto";}, 10000);
+	btn.style.left="auto";}, 7500);
 	
 	setTimeout(function() {var btn = document.getElementById("to_index_btn");
 	btn.style.position="relative";
 	btn.style.zIndex=4;
 	btn.style.top="auto";
-	btn.style.left="auto";}, 10000);
+	btn.style.left="auto";}, 7500);
 }
-
-
-
-
 
 function ShowImages3() {  // eslint-disable-line no-unused-vars
 	setTimeout(function() {var img2 = document.getElementById("img2");
-	img2.style.zIndex=3; img2.style.animationPlayState="running"}, 3000);
+	img2.style.zIndex=3; img2.style.animationPlayState="running"}, 1500);
 	
 	
 	setTimeout(function() {var x = document.getElementById("text"); x.style.zIndex=6;
-	x.style.animationPlayState = "running";}, 6000);
+	x.style.animationPlayState = "running";}, 4000);
 	
 	setTimeout(function() {var x = document.getElementById("text_bg"); x.style.zIndex=5;
-	x.style.animationPlayState = "running";}, 6000);
+	x.style.animationPlayState = "running";}, 4000);
 	
 	setTimeout(function() {var btn = document.getElementById("back_btn");
 	btn.style.position="relative";
 	btn.style.zIndex=4;
 	btn.style.top="auto";
-	btn.style.left="auto";}, 7000);
+	btn.style.left="auto";}, 6500);
 	
 	setTimeout(function() {var btn = document.getElementById("to_index_btn");
 	btn.style.position="relative";
 	btn.style.zIndex=4;
 	btn.style.top="auto";
-	btn.style.left="auto";}, 7000);
+	btn.style.left="auto";}, 6500);
 	
 	setTimeout(function() {var next_btn = document.getElementById("next_btn");
 	next_btn.style.position="relative";
 	next_btn.style.zIndex=4;
 	next_btn.style.top="auto";
-	next_btn.style.left="auto";}, 7000);
+	next_btn.style.left="auto";}, 6500);
 }
-
-
